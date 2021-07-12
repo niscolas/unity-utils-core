dc7354bd8c8b74287fd720a0aca4efbbc120119e
Nícolas Catarina Parreiras <nic.cp@hotmail.com>
2021-07-12 15:28:06 -0300

    updates plugin

--- Scripts/Shader/ShaderNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UnityUtils.Shader
-{
-	public static class ShaderNames
-	{
-		private const string UrpPrefix = "Universal Render Pipeline/";
-		public const string UrpLit = UrpPrefix + "/Lit";
-	}
-}
