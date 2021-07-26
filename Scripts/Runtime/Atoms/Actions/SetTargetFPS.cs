fded271e90c8627ca5b2870999ab9858fb4c5569
niscolas <nic.cp@hotmail.com>
2021-07-26 19:48:22 -0300

    updates plugin

--- Scripts/Runtime/Atoms/Actions/SetTargetFPS.cs
+++ Scripts/Runtime/Atoms/Actions/SetTargetFPS.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 namespace UnityAtomsUtils
 {
-	[CreateAssetMenu(menuName = Constants.ActionsCreateAssetMenuPath + "(int) => Set Target FPS")]
-	public class SetTargetFPS : AtomAction<int>
-	{
-		[SerializeField]
-		private IntReference _fixedTargetFps;
+    [CreateAssetMenu(menuName = Constants.ActionsCreateAssetMenuPath + "(int) => Set Target FPS")]
+    public class SetTargetFPS : AtomAction<int>
+    {
+        [SerializeField]
+        private IntReference _fixedTargetFps;
 
-		public override void Do()
-		{
-			Do(_fixedTargetFps);
-		}
+        public override void Do()
+        {
+            Do(_fixedTargetFps);
+        }
 
-		public override void Do(int entry)
-		{
-			if (Application.targetFrameRate == entry) return;
-			Application.targetFrameRate = entry;
-		}
-	}
+        public override void Do(int entry)
+        {
+            if (Application.targetFrameRate == entry) return;
+            Application.targetFrameRate = entry;
+        }
+    }
 }
