051dfb5f4730436945942330d6888b82c80f1f50
niscolas <nic.cp@hotmail.com>
2021-08-04 21:09:24 -0300

    starts importing methods from UnityUtils.UnityAtoms Root

--- Scripts/Runtime/Root/RootExtensions.cs
+++ Scripts/Runtime/Root/RootExtensions.cs
@@ -1,49 +1,66 @@
-﻿using UnityEngine;
+﻿using niscolas.UnityExtensions;
+using UnityEngine;
 
 namespace niscolas.UnityUtils.Core
 {
-	public static class RootExtensions
-	{
-		public static bool IsRoot(this Component component)
-		{
-			return component.gameObject.IsRoot();
-		}
+    public static class RootExtensions
+    {
+        public static bool IsRoot(this Component component)
+        {
+            return component.gameObject.IsRoot();
+        }
 
-		public static bool IsRoot(this GameObject gameObject)
-		{
-			return Core.Root.CheckIsRoot(gameObject);
-		}
+        public static bool IsRoot(this GameObject gameObject)
+        {
+            return Core.Root.CheckIsRoot(gameObject);
+        }
 
-		public static GameObject Root(this Component component)
-		{
-			return component.gameObject.Root();
-		}
+        public static GameObject Root(this Component component)
+        {
+            return component.gameObject.Root();
+        }
 
-		public static GameObject Root(this GameObject gameObject)
-		{
-			if (gameObject.IsRoot())
-			{
-				return gameObject;
-			}
+        public static GameObject Root(this GameObject gameObject)
+        {
+            if (gameObject.IsRoot())
+            {
+                return gameObject;
+            }
 
-			Root root = gameObject.GetComponentInParent<Root>();
-			
-			if (root)
-			{
-				return root.gameObject;
-			}
+            Root root = gameObject.GetComponentInParent<Root>();
 
-			return null;
-		}
+            if (root)
+            {
+                return root.gameObject;
+            }
 
-		public static T GetComponentFromRoot<T>(this Component component)
-		{
-			return GetComponentFromRoot<T>(component.gameObject);
-		}
+            return null;
+        }
 
-		public static T GetComponentFromRoot<T>(this GameObject gameObject)
-		{
-			return gameObject.Root().GetComponentInChildren<T>();
-		}
-	}
+        public static bool TryFindRoot(this GameObject gameObject, out GameObject root)
+        {
+            root = gameObject.Root();
+            return root;
+        }
+
+        public static T GetComponentFromRoot<T>(this Component component)
+        {
+            return GetComponentFromRoot<T>(component.gameObject);
+        }
+
+        public static T GetComponentFromRoot<T>(this GameObject gameObject)
+        {
+            return gameObject.Root().GetComponentInChildren<T>();
+        }
+
+        public static T IfNullGetComponentFromRoot<T>(this GameObject gameObject, T component) where T : class
+        {
+            if (!component.IsUnityNull())
+            {
+                return component;
+            }
+
+            return gameObject.GetComponentFromRoot<T>();
+        }
+    }
 }
