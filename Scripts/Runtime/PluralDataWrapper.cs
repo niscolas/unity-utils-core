b95ce22d0d945caf6e51a5d17e276031ab989fc9
niscolas <nic.cp@hotmail.com>
2021-08-25 18:43:16 -0300

    updates PluralDataWrapper

--- Scripts/Runtime/PluralDataWrapper.cs
+++ Scripts/Runtime/PluralDataWrapper.cs
@@ -4,61 +4,10 @@ using niscolas.UnityExtensions;
 
 namespace BestLostNFound
 {
-    public abstract class PluralDataWrapper<T>
+    public abstract class PluralDataWrapper<T> : List<T>
     {
-        public PluralDataWrapper()
-        {
-            Internal_Content = new List<T>();
-        }
+        public PluralDataWrapper(IEnumerable<T> content) : base(content) { }
 
-        public PluralDataWrapper(IEnumerable<T> content)
-        {
-            Internal_Content = content.AsList();
-        }
-
-        public PluralDataWrapper(params T[] content)
-        {
-            Internal_Content = content.AsList();
-        }
-
-        public virtual bool IsValid => Internal_Content.IsNotNullOrEmpty();
-
-        public virtual int Count => Internal_Content.Count;
-
-        protected virtual List<T> Internal_Content { get; set; }
-
-        public void Add(T data)
-        {
-            Internal_Content.Add(data);
-        }
-
-        public void Join(PluralDataWrapper<T> other)
-        {
-            Internal_Content.AddRange(other.Internal_Content);
-        }
-
-        public void Clear()
-        {
-            Internal_Content.Clear();
-        }
-    }
-
-    public abstract class EnumerablePluralDataWrapper<T> : PluralDataWrapper<T>, IEnumerable<T>
-    {
-        public EnumerablePluralDataWrapper() { }
-
-        public EnumerablePluralDataWrapper(IEnumerable<T> content) : base(content) { }
-
-        public EnumerablePluralDataWrapper(params T[] content) : base(content) { }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return Internal_Content.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        public PluralDataWrapper(params T[] content) : base(content) { }
     }
 }
