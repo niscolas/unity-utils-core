7f614ce138e9cd31191b2bb577480b6a8914a6e3
niscolas <nic.cp@hotmail.com>
2021-09-20 18:18:24 -0300

    adds AssetDatabaseUtility

---
+++ Scripts/Editor/AssetDatabaseUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace niscolas.UnityUtils.Core.Editor
+{
+    public static class AssetDatabaseUtility
+    {
+        public static T FindFirstAssetOfType<T>() where T : Object
+        {
+            T result = FindAllAssetsOfType<T>().FirstOrDefault();
+
+            return result;
+        }
+
+        public static IEnumerable<T> FindAllAssetsOfType<T>() where T : Object
+        {
+            return (IEnumerable<T>) FindAllAssetsOfType(typeof(T));
+        }
+
+        public static IEnumerable<Object> FindAllAssetsOfType(Type assetType)
+        {
+            string typeName = assetType.Name;
+
+            string[] assetGuids = AssetDatabase.FindAssets($"t:{typeName}");
+            IEnumerable<string> assetPaths = assetGuids.Select(AssetDatabase.GUIDToAssetPath);
+
+            IEnumerable<Object> assets = assetPaths.Select(
+                assetPath => AssetDatabase.LoadAssetAtPath(assetPath, assetType));
+
+            return assets;
+        }
+    }
+}
