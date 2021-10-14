a63ba2e2c45b6b34eb742d2dc82b529072da1587
niscolas <nic.cp@hotmail.com>
2021-10-14 14:06:08 -0300

    adds CachedMonoBehaviour

---
+++ Scripts/Runtime/CachedMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace niscolas.UnityUtils.Core
+{
+    public class CachedMonoBehaviour : MonoBehaviour
+    {
+        protected GameObject _gameObject;
+        protected Transform _transform;
+
+        private void Awake()
+        {
+            _gameObject = gameObject;
+            _transform = transform;
+        }
+
+        protected virtual void Inner_Awake() { }
+    }
+}
