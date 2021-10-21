6f6689d6ddfcfeb0314657f85d559a7fd0c73fca
niscolas <nic.cp@hotmail.com>
2021-10-20 22:27:40 -0300

    adds IConstraintExtensions

---
+++ Scripts/Runtime/Extensions/IConstraintExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+
+namespace niscolas.UnityUtils.Core
+{
+    public static class IConstraintExtensions
+    {
+        public static int AddSourceAndSetActive(
+            this IConstraint constraint,
+            Transform sourceTransform,
+            float weight = 1f,
+            bool activeState = true)
+        {
+            int sourceIndex = constraint.AddSource(
+                new ConstraintSource
+                {
+                    sourceTransform = sourceTransform,
+                    weight = weight
+                });
+            constraint.constraintActive = activeState;
+
+            return sourceIndex;
+        }
+    }
+}
