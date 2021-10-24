8a0cd46dde341dcd0af1b650b3330212519104a9
niscolas <nic.cp@hotmail.com>
2021-10-24 14:27:19 -0300

    adds RecompileScripts feature

---
+++ Scripts/Editor/Features/RecompileScripts.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+
+namespace niscolas.UnityUtils.Core.Editor
+{
+    public class RecompileScripts
+    {
+        [MenuItem(Constants.ToolsMenuItemPrefix + "Recompile Scripts")]
+        public static void Do()
+        {
+            CompilationPipeline.RequestScriptCompilation();
+        }
+    }
+}
