04872c63948b9418ace2cb604eb339352bd8d636
niscolas <nic.cp@hotmail.com>
2021-11-09 17:07:28 -0300

    updates ExplosionUtility

--- Scripts/Runtime/ExplosionUtility.cs
+++ Scripts/Runtime/ExplosionUtility.cs
@@ -1,57 +1,69 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace niscolas.UnityUtils.Core
 {
     public static class ExplosionUtility
     {
         public static void SimpleExplosion(
-            Vector3 explosionPosition, 
+            Vector3 explosionPosition,
             float targetCheckRadius,
             Collider[] results,
             int layerMask,
             float explosionForce,
             float explosionRadius,
             float upwardsModifier = 0,
-            ForceMode forceMode = ForceMode.Force)
+            ForceMode forceMode = ForceMode.Force,
+            params Collider[] collidersToExclude)
         {
             int resultCount = Physics.OverlapSphereNonAlloc(
-                    explosionPosition, targetCheckRadius, results, layerMask);
+                explosionPosition, targetCheckRadius, results, layerMask);
+
+            bool shouldExcludeAnyCollider = collidersToExclude != null &&
+                                            collidersToExclude.Length > 0;
 
             for (int i = 0; i < resultCount; i++)
             {
+                if (!shouldExcludeAnyCollider ||
+                    !results[i] ||
+                    collidersToExclude.Contains(results[i]))
+                {
+                    continue;
+                }
+
                 ExplodeGameObject(
-                    results[i].gameObject, 
-                    explosionPosition, 
-                    explosionForce, 
+                    results[i].gameObject,
+                    explosionPosition,
+                    explosionForce,
                     explosionRadius,
                     upwardsModifier,
                     forceMode);
             }
         }
-        
+
         public static void ExplodeGameObject(
-            GameObject gameObject, 
-            Vector3 explosionPosition, 
-            float explosionForce, 
+            GameObject gameObject,
+            Vector3 explosionPosition,
+            float explosionForce,
             float explosionRadius,
             float upwardsModifier = 0,
             ForceMode forceMode = ForceMode.Force)
         {
             Rigidbody rigidbody = gameObject.GetComponentFromRoot<Rigidbody>();
-            
+
             ExplodeRigidbody(
-                rigidbody, 
-                explosionPosition, 
-                explosionForce, 
+                rigidbody,
+                explosionPosition,
+                explosionForce,
                 explosionRadius,
                 upwardsModifier,
                 forceMode);
         }
-        
+
         public static void ExplodeRigidbody(
-            Rigidbody rigidbody, 
-            Vector3 explosionPosition, 
-            float explosionForce, 
+            Rigidbody rigidbody,
+            Vector3 explosionPosition,
+            float explosionForce,
             float explosionRadius,
             float upwardsModifier = 0,
             ForceMode forceMode = ForceMode.Force)
@@ -62,11 +74,11 @@ namespace niscolas.UnityUtils.Core
             }
 
             rigidbody.AddExplosionForce(
-                    explosionForce, 
-                    explosionPosition, 
-                    explosionRadius, 
-                    upwardsModifier,
-                    forceMode);
+                explosionForce,
+                explosionPosition,
+                explosionRadius,
+                upwardsModifier,
+                forceMode);
         }
     }
 }
