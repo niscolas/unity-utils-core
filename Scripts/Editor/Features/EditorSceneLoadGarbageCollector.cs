4923348567e34e952f3b88c11dfb41f05f9be2ad
niscolas <nic.cp@hotmail.com>
2021-11-11 14:09:49 -0300

    adds EditorSceneLoadGarbageCollector

---
+++ Scripts/Editor/Features/EditorSceneLoadGarbageCollector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace niscolas.UnityUtils.Core.Editor
+{
+    public static class EditorSceneLoadGarbageCollector
+    {
+        [InitializeOnLoadMethod]
+        private static void Init()
+        {
+            EditorSceneManager.sceneOpened += OnSceneOpened;
+        }
+
+        private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
+        {
+            GarbageCollect();
+        }
+
+        [MenuItem(Constants.ToolsMenuItemPrefix + "Force Garbage Collection")]
+        private static void GarbageCollect()
+        {
+            EditorUtility.UnloadUnusedAssetsImmediate();
+            GC.Collect();
+        }
+    }
+}
