e1c7284bc29a9ff99f6cf689409b8f07403396d9
niscolas <nic.cp@hotmail.com>
2021-11-12 14:29:30 -0300

    adds AutoTriggerMonoBehaviour

---
+++ Scripts/Runtime/MonoHooks/AutoTriggerMonoBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using niscolas.UnityUtils.Core;
+using UnityEngine;
+using UnityUtils;
+
+namespace niscolas.UnityUtils.Core
+{
+    public abstract class AutoTriggerMonoBehaviour : CachedMonoBehaviour
+    {
+        [SerializeField]
+        private MonoCallbackType _autoTriggerCallback;
+
+        protected abstract void Do();
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            MonoLifecycleHooksManager.AutoTrigger(_gameObject, Do, _autoTriggerCallback);
+        }
+    }
+}
