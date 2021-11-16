b9cfd8716adcb1cae8ce11fa6e15e7bccab6bf3c
niscolas <nic.cp@hotmail.com>
2021-11-16 15:13:24 -0300

    adds CreateBlendTreeAsset

---
+++ Scripts/Editor/Features/CreateBlendTreeAsset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace niscolas.UnityUtils.Core.Editor
+{
+    public static class CreateBlendTreeAsset
+    {
+        [MenuItem(Constants.ToolsMenuItemPrefix + "Create BlendTree")]
+        private static void CreateBlendTree()
+        {
+            if (!(Selection.activeObject is BlendTree blendTree))
+            {
+                return;
+            }
+
+            blendTree = Object.Instantiate<BlendTree>(blendTree);
+            AssetDatabase.CreateAsset(blendTree, $"Assets/{blendTree.name}.asset");
+        }
+    }
+}
