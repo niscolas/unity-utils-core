f892444ef7de2c0073de9edc966f4124c5c28466
niscolas <nic.cp@hotmail.com>
2022-01-13 10:38:51 -0300

    refactor: removes UniTask and UnityAtoms related files

--- Scripts/Runtime/Constants.cs
+++ Scripts/Runtime/Constants.cs
@@ -4,12 +4,13 @@
     {
         public const string AssetMenuItemPrefix = "Assets/" + NiscolasTag + "/";
         public const string AddComponentMenuPrefix = CoreCreateAssetMenuPrefix;
-        public const string CoreCreateAssetMenuPrefix = NiscolasCreateAssetMenuPrefix + UnityUtilsCoreTag + "/";
+        public const string CoreCreateAssetMenuPrefix = UnityUtilsCreateAssetMenuPrefix + UnityUtilsCoreTag + "/";
         public const int CreateAssetMenuOrder = -100;
         public const string UnityUtilsCoreTag = "[Core]";
         public const string NiscolasCreateAssetMenuPrefix = NiscolasTag + "/";
         public const string NiscolasTag = "[niscolas]";
         public const string ToolsMenuItemPrefix = "Tools/" + NiscolasTag + "/";
+        public const string UnityUtilsAddComponentMenuPrefix = UnityUtilsCreateAssetMenuPrefix;
         public const string UnityUtilsCreateAssetMenuPrefix = NiscolasCreateAssetMenuPrefix + UnityUtilsTag + "/";
         public const string UnityUtilsTag = "[Unity Utils]";
     }
